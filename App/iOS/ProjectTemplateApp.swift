8af41e9962e5b11d5f2d76e896fb06cad9cbd677
Daichi Hayashi <daichikuwa0618@gmail.com>
2023-02-13 16:42:40 +0900

    Add Production / Staging xcodeproj

---
+++ App/iOS/ProjectTemplateApp.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectTemplateApp.swift
+//
+
+import SwiftUI
+
+@main
+struct ProjectTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
